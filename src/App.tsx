945cbcd1cee4dc56741dc5ba4ca810c640101c41
Jacob Pfeifer <jacobdpfeifer@gmail.com>
2026-06-30 12:11:02 -0600

    Add redirect from root path to /provision in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { PatientView } from "./views/PatientView.tsx";
 import { DeskView } from "./views/DeskView.tsx";
 import { ProvisionTag } from "./views/ProvisionTag.tsx";
@@ -7,6 +7,7 @@ export function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/provision" replace />} />
         <Route path="/o/:officeId" element={<PatientView />} />
         <Route path="/desk/:officeId" element={<DeskView />} />
         <Route path="/provision" element={<ProvisionTag />} />
